d757a7666326cce77be726faa2f6674e3e69f40b
CollinFisk <beatsaber6000@gmail.com>
2026-09-18 12:22:27 -0700

    Complete Weapon/Damsge systems

--- Collin_3d Unity Project/Assets/Scripts/PlayerController.cs
+++ Collin_3d Unity Project/Assets/Scripts/PlayerController.cs
@@ -1,13 +1,22 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
+    public int health = 5;
+    public int maxHealth = 5;
+
     public float speed = 5.0f;
-    public float jumpheight = 10.0f;
-    public float jumpDetectDistance = 1.0f;
+    public float jumpHeight = 10.0f;
+    public float jumpDetectDistance = 1f;
     public float interactDistance = 5f;
-    
+    public float fusionDmgInterval = 1;
+
+    public bool attacking = false;
+    public bool fusionDmg = false;
+
     Ray jumpRay;
     Ray interactRay;
     RaycastHit interactHit;
@@ -15,17 +24,18 @@ public class PlayerController : MonoBehaviour
 
     public Weapon currentWeapon;
 
-    public Transform weaponSlot;
     Camera playerCam;
+    public Transform weaponSlot;
     PlayerInput input;
-    Rigidbody Rb;
-    GameObject pickupObj;
+    Rigidbody rb;
+    public GameObject pickupObj;
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         input = GetComponent<PlayerInput>();
-        Rb = GetComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody>();
         jumpRay = new Ray();
         playerCam = Camera.main;
 
@@ -36,9 +46,22 @@ public class PlayerController : MonoBehaviour
         Cursor.visible = false;
     }
 
+    private void FixedUpdate()
+    {
+        Quaternion playerRotation = Quaternion.identity;
+        playerRotation.y = playerCam.transform.rotation.y;
+        playerRotation.w = playerCam.transform.rotation.w;
+        transform.rotation = playerRotation;
+    }
+
     // Update is called once per frame
     void Update()
     {
+        if (health <= 0)
+        {
+
+        }
+
         jumpRay.origin = transform.position;
         jumpRay.direction = -transform.up;
 
@@ -47,21 +70,31 @@ public class PlayerController : MonoBehaviour
 
         if (Physics.Raycast(interactRay, out interactHit, interactDistance))
         {
-            if (interactHit.collider.tag == "Weapon")
+            if (interactHit.collider.tag == "Weapon" || interactHit.collider.tag == "Ammo")
             {
                 pickupObj = interactHit.collider.gameObject;
             }
-            else pickupObj = null;
+
+            else
+                pickupObj = null;
         }
         else
             pickupObj = null;
-        Vector3 tempMove = Rb.linearVelocity;
+
+        if (currentWeapon)
+            if (currentWeapon.holdToAttack && attacking)
+                currentWeapon.fire();
+
+        Vector3 tempMove = rb.linearVelocity;
 
         tempMove.x = moveInput.x * speed;
         tempMove.z = moveInput.y * speed;
 
-        Rb.linearVelocity = tempMove;
+        rb.linearVelocity = (tempMove.x * transform.right) +
+                            (tempMove.y * transform.up) +
+                            (tempMove.z * transform.forward);
     }
+
     public void Move(InputAction.CallbackContext context)
     {
         moveInput = context.ReadValue<Vector2>();
@@ -69,44 +102,149 @@ public class PlayerController : MonoBehaviour
 
     public void Jump()
     {
-        if (Physics.Raycast(jumpRay.origin, jumpRay.direction, jumpDetectDistance))
-            Rb.AddForce(transform.up * jumpheight, ForceMode.Impulse);
-
+        if (Physics.Raycast(jumpRay, jumpDetectDistance))
+            rb.AddForce(transform.up * jumpHeight, ForceMode.Impulse);
     }
 
     public void Interact(InputAction.CallbackContext context)
     {
         if (context.ReadValueAsButton())
         {
-            if (pickupObj.tag == "Weapon")
+            if (pickupObj)
             {
-                pickupObj.GetComponent<Weapon>().equip(this);
+                if (pickupObj.tag == "Weapon")
+                {
+                    pickupObj.GetComponent<Weapon>().equip(this);
+                }
+
+                // Interact to pickup ammo
+                /*
+                if (pickupObj.tag == "Ammo" && currentWeapon && currentWeapon.ammo < currentWeapon.maxAmmo)
+                {
+                    int refillAmt = currentWeapon.ammo + currentWeapon.ammoRefill;
+
+                    if (refillAmt >= currentWeapon.maxAmmo)
+                    {
+                        currentWeapon.ammo = currentWeapon.maxAmmo;
+                    }
+                    else
+                        currentWeapon.ammo += currentWeapon.ammoRefill;
+
+                    Destroy(pickupObj);
+                }
+                */
 
                 pickupObj = null;
-             
             }
             else if (currentWeapon)
                 Reload();
-            
         }
     }
+
     public void Reload()
     {
         if (currentWeapon)
-            if (!currentWeapon.reloading) ;
-                 currentWeapon.reload();
+            if (!currentWeapon.reloading)
+                currentWeapon.reload();
     }
+
     public void Attack(InputAction.CallbackContext context)
     {
         if (currentWeapon)
         {
-            if (context.ReadValueAsButton())
-                attacking = true;
+            if (currentWeapon.holdToAttack)
+            {
+                if (context.ReadValueAsButton())
+                    attacking = true;
+                else
+                    attacking = false;
+            }
+
+            else if (context.ReadValueAsButton())
+                currentWeapon.fire();
+        }
+    }
+
+    public void DropWeapon()
+    {
+        if (currentWeapon)
+        {
+            currentWeapon.GetComponent<Weapon>().unequip();
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ammo" && currentWeapon && currentWeapon.ammo < currentWeapon.maxAmmo)
+        {
+            int refillAmt = currentWeapon.ammo + currentWeapon.ammoRefill;
+
+            if (refillAmt >= currentWeapon.maxAmmo)
+            {
+                currentWeapon.ammo = currentWeapon.maxAmmo;
+            }
             else
-                attacking = false;
+                currentWeapon.ammo += currentWeapon.ammoRefill;
+
+            Destroy(collision.gameObject);
         }
 
-        else if (context.ReadValueAsButton())
-            currentWeapon.fire();
+        if (collision.gameObject.tag == "Hazard")
+        {
+            health--;
+        }
+
+        if (collision.gameObject.tag == "FusionHazard")
+        {
+            health--;
+        }
+
+        if (collision.gameObject.tag == "Health" && health < maxHealth)
+        {
+            health++;
+
+            Destroy(collision.gameObject);
+        }
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.tag == "FusionHazard")
+        {
+            if (!fusionDmg)
+            {
+                StartCoroutine("fusionDmgCooldown");
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "LevelEnd")
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "FusionHazard")
+        {
+            if (fusionDmg)
+            {
+                StopCoroutine("fusionDmgCooldown");
+                fusionDmg = false;
+            }
+        }
+    }
+
+    IEnumerator fusionDmgCooldown()
+    {
+        fusionDmg = true;
+
+        yield return new WaitForSeconds(fusionDmgInterval);
+
+        health--;
+        fusionDmg = false;
     }
 }
--- Collin_3d Unity Project/Assets/Scripts/Weapon.cs
+++ Collin_3d Unity Project/Assets/Scripts/Weapon.cs
@@ -1,5 +1,4 @@
 using System.Collections;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class Weapon : MonoBehaviour
@@ -8,12 +7,12 @@ public class Weapon : MonoBehaviour
 
     [Header("Object Refrences")]
     public GameObject projectile;
-    public Transform firepoint;
+    public Transform firePoint;
     public Camera firingDirection;
 
     [Header("Meta Attributes")]
     public bool canFire = true;
-    public bool holdtoAttack = true;
+    public bool holdToAttack = true;
     public bool reloading = false;
     public int weaponID;
     public string weaponName;
@@ -23,28 +22,23 @@ public class Weapon : MonoBehaviour
     public float projVelocity;
     public float reloadCooldown;
     public float rof;
-    public float firePoint;
     public int fireModes;
     public int currentFireMode;
     public int clip;
     public int clipSize;
 
-    [Header("Weapon Stats")]
+    [Header("Ammo Stats")]
     public int ammo;
     public int maxAmmo;
     public int ammoRefill;
-        // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        firepoint = transform.GetChild(0);
+        firePoint = transform.GetChild(0);
         firingDirection = Camera.main;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
     public void equip(PlayerController p)
     {
         player = p;
@@ -54,13 +48,20 @@ public class Weapon : MonoBehaviour
         transform.SetPositionAndRotation(player.weaponSlot.position, player.weaponSlot.rotation);
         transform.SetParent(player.weaponSlot);
 
-        GetComponent<Rigidbody>().isKinematic = false;
-        GetComponent <Collider>().isTrigger = false;
-
-        player = null;
+        GetComponent<Rigidbody>().isKinematic = true;
+        GetComponent<Collider>().isTrigger = true;
     }
+
     public void unequip()
     {
+        player.currentWeapon = null;
+
+        transform.SetParent(null);
+
+        GetComponent<Rigidbody>().isKinematic = false;
+        GetComponent<Collider>().isTrigger = false;
+
+        player = null;
 
     }
 
@@ -68,6 +69,7 @@ public class Weapon : MonoBehaviour
     {
         if (clip >= clipSize)
             return;
+
         int reloadCount = clipSize - clip;
 
         if (ammo < reloadCount)
@@ -76,42 +78,43 @@ public class Weapon : MonoBehaviour
             ammo = 0;
         }
         else
-        { 
-
-        clip += reloadCount;
-        ammo -= reloadCount;
-        }
-    }
-
-public void fire()
-    {
-    if (clip > 0 && canFire && !reloading)
         {
-        clip--;
-
-        GameObject p = Instantiate(projectile, firePoint.position, firePoint.rotation);
-        p.GetComponent<Rigidbody>().AddForce(firingDirection.transform.forward * projVelocity);
-        OnDestroy(p, projLifespan)
-        
+            clip += reloadCount;
+            ammo -= reloadCount;
         }
+
+        reloading = true;
+        canFire = false;
+        StartCoroutine("reloadingCooldown");
     }
 
-    
-   
-    IEnumerator burstDuration()
+    public void fire()
     {
+        if (clip > 0 && canFire && !reloading)
+        {
+            clip--;
 
+            GameObject p = Instantiate(projectile, firePoint.position, firePoint.rotation);
+            p.GetComponent<Rigidbody>().AddForce(firingDirection.transform.forward * projVelocity);
+            Destroy(p, projLifespan);
+            canFire = false;
+            StartCoroutine("cooldownFire");
+        }
     }
 
     IEnumerator cooldownFire()
     {
-        yield return new WaitForSeconds(rof)
+        yield return new WaitForSeconds(rof);
 
+        if (clip > 0)
+            canFire = true;
     }
-    IEnumerator reloadingcCooldown()
+
+    IEnumerator reloadingCooldown()
     {
+        yield return new WaitForSeconds(reloadCooldown);
 
+        reloading = false;
+        canFire = true;
     }
-
-    
 }
